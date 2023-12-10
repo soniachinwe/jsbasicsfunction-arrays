0f2d524e903d1d9fe0bf15a9722c2243c215baea
soniachinwe <soniachinwe176@gmail.com>
2023-12-10 17:06:53 +0000

    js basics function, parameters and array

---
+++ script.js
@@ -0,0 +1,17 @@
+/*This is function that accepts two parameters. The firt parameter
+is a must be a word while the second parameter is a letter. The function
+when called searches for the letter in the word and matches it
+with the array number where it was found.*/
+function match(word, letter) {
+    for(var i = 0; i < word.length; i++) {
+        if(word[i] == letter) {
+            console.log('letter', letter, 'found at array number', i, 'of', word)
+        } else {
+            console.log('No match found at', i)
+        }
+    }
+}
+
+match("pipe", "p")
+match('sand', 'a')
+
